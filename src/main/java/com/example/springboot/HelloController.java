81d5487135a12de986b9c3b19ccc3d711e1871bc
sodhiyas <sodhiyas@vmware.com>
2022-02-16 04:00:02 -0500

    1645002001

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + 1644994801!";
+		return "Greetings from Spring Boot + Tanzu + 1645002001!";
 	}
 
 }
