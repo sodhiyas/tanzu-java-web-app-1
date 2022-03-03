18648c7593e2bce17626734aa36d01c49b970344
shwethaks1 <94036575+shwethaks1@users.noreply.github.com>
2022-03-03 16:54:14 +0530

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu! welcome"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu! "));
     }
 }
