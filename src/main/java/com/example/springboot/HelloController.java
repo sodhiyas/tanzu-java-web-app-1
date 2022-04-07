2b334c6e71583b0b11e413b3dcd64fd07eb382a1
sodhiyas <97176778+sodhiyas@users.noreply.github.com>
2022-04-07 23:02:59 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + 1645016401!";
+		return "Greetings from Spring Boot + Tanzu + 1645016409!";
 	}
 
 }
